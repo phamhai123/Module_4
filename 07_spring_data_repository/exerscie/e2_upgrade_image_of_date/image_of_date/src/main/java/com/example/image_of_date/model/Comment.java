09bbe058ca4ad36e89fb7493c5705ce999be4376
hai <hai.com>
2023-07-31 18:56:57 +0700

    up

---
+++ 07_spring_data_repository/exerscie/e2_upgrade_image_of_date/image_of_date/src/main/java/com/example/image_of_date/model/Comment.java
@@ -0,0 +1,68 @@
+package com.example.image_of_date.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int rate;
+    private String author;
+    private String feedback;
+    private int likeCount;
+
+    public Comment() {
+    }
+
+    public Comment(int id, int rate, String author, String feedback, int likeCount) {
+        this.id = id;
+        this.rate = rate;
+        this.author = author;
+        this.feedback = feedback;
+        this.likeCount = likeCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+}
---
+++ 07_spring_data_repository/exerscie/e2_upgrade_image_of_date/image_of_date/src/test/java/com/example/image_of_date/ImageOfDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.image_of_date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageOfDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
